acfe82c2f8c87db1db816389e817834a1249a740
lukanthrope <bydlocoder228@gmail.com>
2020-12-14 15:28:32 +0200

    building graph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Graph } from "react-d3-graph";
+import TextField from '@material-ui/core/TextField';
 
-function App() {
+import TSP from './algorithm';
+
+import './App.css';
+ 
+const myConfig = {
+    nodeHighlightBehavior: true,
+    node: {
+        color: "green",
+        size: 300,
+        highlightStrokeColor: "red",
+    },
+    link: {
+        highlightColor: "lightblue",
+    },
+};
+
+
+const initialMatrix = [
+  [0, 2, 7, 0, 0, 0, 0],
+  [2, 0, 0, 3, 3, 4, 0],
+  [7, 0, 0, 1, 0, 0, 0],
+  [0, 3, 7, 0, 5, 1, 0],
+  [0, 3, 0, 5, 0, 0, 5],
+  [0, 4, 0, 1, 0, 0, 2],
+  [0, 0, 0, 0, 5, 2, 0]
+];
+
+function App() { 
+  const [matrix, setMatrix] = useState(initialMatrix);
+  const [schema, setSchema] = useState(null);
+  const [minPath, setMinPath] = useState(null);
+
+  useEffect(() => {
+    const tsp = new TSP(matrix);
+    setSchema(tsp.solve());
+    setMinPath(tsp.path);
+  }, [matrix]);
+
+  const handleChange = (e, i, j) => {
+    const m = [...matrix];
+    m[i][j] = Number(e.target.value);
+    setMatrix(m);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {schema && (
+          <Graph
+            id="graph-id"
+            data={schema}
+            config={myConfig}
+          />
+        )}
+        {minPath && <h2>Min path{minPath}</h2>}
+      </div>
+      <div className="form-container">
+        {
+          matrix.map((r, i) => (
+            <div key={i}>
+              {r.map((el, index) =>
+                <TextField
+                  key={index}
+                  value={el}
+                  id="standard-required"
+                  disabled={i === index}
+                  onChange={(e) => handleChange(e, i, index)}
+                  />
+              )}
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/algorithm/index.js
@@ -0,0 +1,82 @@
+class TSP {
+  matrix = [];
+  path = 0;
+  len = 0;
+
+  constructor(matrix) {
+    console.log(matrix)
+    this.matrix = matrix;
+    this.len = matrix.length;
+  }
+
+  get getMatrix() {
+    return this.matrix;
+  }
+
+  minKey(key, mstSet) {
+    let min = Infinity, min_index;  
+  
+    for (let v = 0; v < this.len; v++) {
+      if (mstSet[v] === false && key[v] < min) {
+        min = key[v];
+        min_index = v;
+      }
+    }
+    return min_index;  
+  } 
+
+  static toString(n) {
+    return (n + 1).toString()
+  }
+
+  _returnResult(parent, graph) {  
+    const res = {
+      nodes: [],
+      links: []
+    };
+
+    parent.forEach((_,index) => {
+      res.nodes.push({ id: TSP.toString(index) });
+    });
+
+    for (let i = 1; i < graph.length; i++) {
+      console.log(parent[i], " - ", i, " \t", graph[i][parent[i]]," \n");
+      this.path += graph[i][parent[i]]; 
+      
+      res.links.push({ source: TSP.toString(parent[i]), target: TSP.toString(i) });
+    } 
+
+    return res;
+  }  
+
+  solve() {
+    const parent = [];  
+    const key = [];    
+    const mstSet = [];  
+
+    for (let i = 0; i < this.len; i++) { 
+      key[i] = Infinity;
+      mstSet[i] = false;
+    }  
+    
+    key[0] = 0;  
+    parent[0] = -1; 
+
+    for (let count = 0; count < this.len - 1; count++) {    
+      let u = this.minKey(key, mstSet);  
+      mstSet[u] = true;  
+  
+      for (let v = 0; v < this.len; v++) {
+        if (this.matrix[u][v] && mstSet[v] === false && this.matrix[u][v] < key[v]) {
+          parent[v] = u;
+          key[v] = this.matrix[u][v];
+        }
+      }
+    }  
+    
+    return this._returnResult(parent, this.matrix);  
+  }  
+
+}
+
+export default TSP;
